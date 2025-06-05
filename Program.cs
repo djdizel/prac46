899fb887d2766888ee01a722ea9a2695afc8dea9
djdizel <yarmisikov@gmail.com>
2025-06-05 10:19:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac46
+{
+    internal class Program
+    {
+        enum Months
+        {
+            Январь,
+            Февраль,
+            Март,
+            Апрель,
+            Май,
+            Июнь,
+            Июль,
+            Август,
+            Сентябрь,
+            Октябрь,
+            Ноябрь,
+            Декабрь
+        }
+        static void Main(string[] args)
+        {
+            //int count = -1;
+            //foreach (var month in Enum.GetValues(typeof(Months)))
+            //{
+            //    count++;
+            //    Console.WriteLine($"{count}\t{month.ToString()}");
+            //}
+            for(int i = 0; i < Enum.GetNames(typeof(Months)).Length; i++)
+            {
+                Console.WriteLine($"{i}\t{Enum.GetName(typeof(Months), i)}");
+            }
+        }
+    }
+}
